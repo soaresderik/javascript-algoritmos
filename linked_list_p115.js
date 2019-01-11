5a0fc8569022f7d1b3e4f6832868fb3d0011eb3d
Andre Soares <codigoaberto.info@gmail.com>
2019-01-11 13:22:38 -0200

    Implementando o insert em qualquer posição. (p.124)

--- linked_list_p115.js
+++ linked_list_p115.js
@@ -23,8 +23,50 @@ function LinkedList() {
 
     length++;
   };
-  this.insert = function(position, element) {};
-  this.removeAt = function(position) {};
+  this.insert = function(position, element) {
+    if (position >= 0 && position <= length) {
+      let node = new Node(element),
+        current = head,
+        previous,
+        index = 0;
+
+      if (position == 0) {
+        node.next = current;
+        head = node;
+      } else {
+        while (index++ < position) {
+          previous = current;
+          current = current.next;
+        }
+
+        node.next = current;
+        previous.next = node;
+      }
+
+      length++;
+      return true;
+    } else return false;
+  };
+  this.removeAt = function(position) {
+    if (position > -1 && position < length) {
+      let current = head,
+        previous,
+        index = 0;
+
+      if (position === 0) head = current.next;
+      else {
+        while (index++ < position) {
+          previous = current;
+          current = current.next;
+        }
+
+        previous.next = current.next;
+      }
+
+      length--;
+      return current.element;
+    } else return null;
+  };
   this.remove = function(element) {};
   this.indexOf = function(element) {};
   this.isEmpty = function() {};
@@ -36,4 +78,9 @@ function LinkedList() {
 let list = new LinkedList();
 list.append(15);
 list.append(10);
-list.append(5);
+list.append(13);
+list.append(11);
+list.append(12);
+list.removeAt(4);
+list.insert(0, 14);
+list.insert(4, 16);
